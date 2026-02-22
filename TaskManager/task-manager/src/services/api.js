8d42f41f050a8aa0cf3be762a0d860884c567fb0
Jonny Vorn Soth <janysoth@gmail.com>
2026-02-22 01:51:29 -0600

    feat: implement user authentication API methods for registration, login, and logout

--- TaskManager/task-manager/src/services/api.js
+++ TaskManager/task-manager/src/services/api.js
@@ -1,11 +1,29 @@
-// src/services/api.js
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Your Express API URL
+  baseURL: 'http://localhost:5295/api/auth', // Update this based on your backend URL
 });
 
-// export const fetchData = async () => {
-//   const response = await api.get('/data');
-//   return response.data;
-// };
+// Register user
+export const register = async (data) => {
+  return await api.post('/register', data);
+};
+
+// Login user
+export const login = async (data) => {
+  return await api.post('/login', data);
+};
+
+// Get user info
+export const getUserInfo = async (token) => {
+  return await api.get('/get-user-info', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+// Logout user
+export const logout = async () => {
+  return await api.post('/logout');
+};
